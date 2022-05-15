793bf4805c4aeaead6c5b1560bffd674c2043743
jemlisana <jemlisana1@gmail.com>
2022-05-15 22:00:55 +0100

    commit changement url bd

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 require('dotenv').config();
-
- DB_STRING='mongodb://localhost:27017/auth'
+DB_STRING='mongodb+srv://sana:sana1234@cluster0.2e6v7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+ //DB_STRING='mongodb://localhost:27017/auth'
     mongoose.connect(DB_STRING, {
         useNewUrlParser: true,
         useUnifiedTopology: true
